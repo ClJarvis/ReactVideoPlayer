a4111863ffae2414e6df53f7766610c45d1022ed
Chris <clj@ollure.com>
2019-07-19 11:09:36 -0500

    adding routes

--- react-hooks-videoplayer-START/src/components/containers/App.js
+++ react-hooks-videoplayer-START/src/components/containers/App.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import VidPlayer from './VidPlayer';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 const App = () => (
-  <VidPlayer />
+  <BrowserRouter>
+  	<Switch>
+  		<Route exact path="/" component={VidPlayer} />
+  		<Route exact path="/:activeVideo"  component={VidPlayer} />
+  	</Switch>
+  </BrowserRouter>
 )
 
 export default App;
